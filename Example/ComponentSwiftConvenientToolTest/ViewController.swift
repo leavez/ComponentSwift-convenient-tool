c2fda58310bdf1b70bccca724d36f6fdcbf3cdad
leavez <gaojiji@gmail.com>
2017-08-27 18:28:20 +0800

    add test demo

---
+++ Example/ComponentSwiftConvenientToolTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ComponentSwiftConvenientToolTest
+//
+//  Created by Gao on 27/08/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import ComponentSwift_ConvenientTool
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
